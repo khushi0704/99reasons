578c84a5d6aa207f2889409ceb32a263cf52837e
Khushi Jain <khushi.jain0704@gmail.com>
2021-03-28 17:13:37 +0530

    updated

--- js/script.js
+++ js/script.js
@@ -1,10 +1,25 @@
-window.onscroll = function () {
+/* window.onscroll = function () {
     scrollRotate();
 };
 
 function scrollRotate() {
     let image = document.getElementById("image8");
     image.style.transform = "rotate(" + window.pageYOffset/10 + "deg)";
-    let imagee = document.getElementById("image9");
-    imagee.style.transform = "rotate(" + window.pageYOffset/7 + "deg)";
+}*/
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("nav");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= 500) {
+    navbar.classList.add("sticky")
+  }
+   else {
+    navbar.classList.remove("sticky");
+  }
 }
